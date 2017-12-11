2c5d152c9474f35a4818185c2b9177da5a353716
Tyler Marshall <tmarsha9@vols.utk.edu>
2017-12-10 23:45:05 -0500

    Add day5 part1 soln
    Signed-off-by: Tyler Marshall <tmarsha9@vols.utk.edu>

---
+++ day5/part1/twisty_trampolines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+f = open("input.txt", "r")
+
+instructions = []
+
+for item in f:
+  instructions.append(int(item))
+
+current_instruction = 0
+instruction_count = 0
+
+while True:
+  try:
+    next_instruction = current_instruction+instructions[current_instruction]
+    instructions[current_instruction]+=1
+    current_instruction = next_instruction
+    instruction_count+=1
+  except IndexError:
+    print instruction_count
+    break
